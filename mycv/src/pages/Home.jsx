f99108ad1c4c813fd292492905e5964e7659e448
Yvan Wildis Ngonetchhinda <yvanngone53@gmail.com>
2025-10-29 23:26:58 +0100

    Add About page with personal information and skills; remove old About component

--- mycv/src/pages/Home.jsx
+++ mycv/src/pages/Home.jsx
@@ -13,11 +13,11 @@ const Home = () => {
     document.body.removeChild(link); // Supprime le lien après le clic
   };
   return (
-    <div className="max-w-[1800px] mx-auto grid grid-cols-6 ">
+    <div className="min-h-screen max-w-[1800px] mx-auto grid grid-cols-1 md:grid-cols-6">
       <div className=" relative md:col-span-2  flex justify-center items-center ">
         <Navbar />
       </div>
-      <div className=" md:p-4 md:flex md:flex-col md:items-center md:justify-center md:h-[95vh] md:text-center sm:space-y-6  col-span-4  md:w-[95%] w-full h-[100vh] flex flex-col justify-center text-center items-center ">
+      <div className="sm:ml-10 md:p-4 md:flex md:flex-col md:items-center md:justify-center md:h-[95vh] md:text-center sm:space-y-6  col-span-4  md:w-[95%] w-full h-[100vh] flex flex-col justify-center text-center items-center ">
         {/* Animation depuis la gauche */}
         <motion.h1
           initial={{ x: -200, opacity: 0 }}
